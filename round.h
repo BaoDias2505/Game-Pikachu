022e9df7f263b70492ee810d68e4f38df9740bd5
Nguyễn Mậu Gia Bảo <105162542+BaoDias2505@users.noreply.github.com>
2022-10-23 22:17:31 +0700

    Create round.h

---
+++ round.h
@@ -0,0 +1,20 @@
+#pragma once
+//#ifndef __ROUND_H__
+//#define __ROUND_H__
+//#endif
+#ifndef __ROUND_H__
+#define __ROUND_H__
+
+#endif
+
+#include "Graphics.h"
+#include "Global.h"
+
+
+void swap(char& a, char& b);
+void vong2(char**& pikachu, int begin, int end);
+
+void vong3(char**& pikachu, int begin, int end);
+
+void vong4(char**& pikachu, int begin, int end);
+void vong5(char**& pikachu, int begin, int end);
