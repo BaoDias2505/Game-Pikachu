fafb2049d5801bd7e035a1cfbfe93fe319b22598
Nguyễn Mậu Gia Bảo <105162542+BaoDias2505@users.noreply.github.com>
2022-10-23 22:17:05 +0700

    Create matching.h

---
+++ matching.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Global.h"
+#include "Graphics.h"
+bool Imatching(toado A, toado B, char** pikachu);
+
+bool ZUmatching(toado A, toado B, char** pikachu);
+
+bool Lmatching(toado A, toado B, char** pikachu);
